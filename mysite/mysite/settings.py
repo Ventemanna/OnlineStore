a23a8e4da8e278608984595d74b01bcb35b5e5ca
venta <ventumanna@gmail.com>
2024-07-03 17:18:37 +0300

    Added STATIS-ROOT and folders for future files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
